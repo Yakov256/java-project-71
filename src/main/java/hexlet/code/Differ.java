3ae6d35ed831f98eed4a7e811483fc362ae7da24
yakov256 <yakov256@yandex.ru>
2023-08-23 11:14:16 +0300

    step 6 Differ.java optimization for codeclimate

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -31,6 +31,25 @@ public class Differ {
         return jsonMap;
     }
 
+    public static String getLineDifferences(String key, String map1Value, String map2Value) {
+        StringBuilder rezStr = new StringBuilder();
+        if (map1Value != null) {
+            if (map2Value == null) {
+                rezStr.append("- " + key + ": " + map1Value + "\n");
+            } else if (map1Value.equals(map2Value)) {
+                rezStr.append("  " + key + ": " + map1Value + "\n");
+            } else if (!map1Value.equals(map2Value)) {
+                rezStr.append("- " + key + ": " + map1Value + "\n");
+                rezStr.append("+ " + key + ": " + map2Value + "\n");
+            }
+        } else {
+            if (map2Value != null) {
+                rezStr.append("+ " + key + ": " + map2Value + "\n");
+            }
+        }
+        return rezStr.toString();
+    }
+
     public static String getMapsDifferences(Map<String, String> map1, Map<String, String> map2) throws ParseException {
 
         // Получаем отсортированный Map с ключами из двух json файлов
@@ -44,14 +63,14 @@ public class Differ {
             String map1Value = map1.get(entry.getKey());
             String map2Value = map2.get(entry.getKey());
 
+            rezStr.append(getLineDifferences(entry.getKey(), map1Value, map2Value));
+            /*
             if (map1Value != null) {
                 if (map2Value == null) {
                     rezStr.append("- " + entry.getKey() + ": " + map1Value + "\n");
-                };
-                if (map1Value.equals(map2Value)) {
+                } else if (map1Value.equals(map2Value)) {
                     rezStr.append("  " + entry.getKey() + ": " + map1Value + "\n");
-                };
-                if (!map1Value.equals(map2Value)) {
+                } else if (!map1Value.equals(map2Value)) {
                     rezStr.append("- " + entry.getKey() + ": " + map1Value + "\n");
                     rezStr.append("+ " + entry.getKey() + ": " + map2Value + "\n");
                 }
@@ -60,9 +79,9 @@ public class Differ {
                     rezStr.append("+ " + entry.getKey() + ": " + map2Value + "\n");
                 }
             }
+             */
         }
         rezStr.append("}\n");
-
         return rezStr.toString();
     }
 
