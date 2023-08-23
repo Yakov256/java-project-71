95920f1b64297c53ba4364f6f76690485d587297
yakov256 <yakov256@yandex.ru>
2023-08-23 21:59:41 +0300

    step 7

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -1,14 +1,22 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLParser;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.yaml.snakeyaml.Yaml;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
+
+import java.io.File;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 public class Differ {
 
@@ -31,14 +39,65 @@ public class Differ {
         return jsonMap;
     }
 
+    private static Map<String, String> readMapFromYmlFile(String filePath) throws IOException, ParseException {
+        System.out.println("Reading JSON from file: " + filePath);
+
+        File file = new File(filePath);
+        YAMLFactory yamlFactory = new YAMLFactory();
+        YAMLParser yamlParser = yamlFactory.createParser(file);
+        JsonToken jsonToken = yamlParser.nextToken();
+        Map<String, String> yamlMap = new HashMap<>();
+        String fieldName = "";
+        String fieldValue = "";
+        while(jsonToken != null) {
+            switch(jsonToken) {
+                //System.out.println("--- " + jsonToken.toString());
+                case FIELD_NAME:fieldName = yamlParser.getText();
+                    System.out.println("Key field: " + yamlParser.getText());
+                    break;
+                case START_OBJECT: System.out.println("Object Started");
+                    break;
+                case END_OBJECT: System.out.println("Object Ended");
+                    break;
+                case START_ARRAY: System.out.println("Array Started");
+                    break;
+                case END_ARRAY: System.out.println("Array Ended");
+                    break;
+                //case VALUE_FALSE:
+                //case VALUE_NULL:
+                //case VALUE_NUMBER_FLOAT:
+                //case VALUE_NUMBER_INT:
+                //case VALUE_STRING:
+                //case VALUE_TRUE:
+                default: fieldValue = yamlParser.getText();
+                    //System.out.println("Key value: " + yamlParser.getText() + " - " + i);
+                    if (!fieldValue.equals("")) {
+                        yamlMap.put(fieldName, fieldValue);
+                        fieldName = "";
+                    }
+                    break;
+            }
+
+            jsonToken = yamlParser.nextToken();
+        }
+        yamlParser.close();
+
+        return yamlMap;
+    }
+
     private static Map<String, String> readMapFromFile(String filePath) throws IOException, ParseException {
+        Map<String, String> jsonMap = new HashMap<>();
 
         System.out.println(filePath);
-        String[] strFileNameAray = filePath.split(".");
-        System.out.println(strFileNameAray.length);
-        //System.out.println(strFileNameAray[strFileNameAray.length - 1]);
+        if (filePath.endsWith("json")) {
+            System.out.println("Обрабатываем json");
+            jsonMap = readMapFromJsonFile(filePath);
+        } else if (filePath.endsWith("yml")) {
+            System.out.println("Обрабатываем yml");
+            jsonMap = readMapFromYmlFile(filePath);
+        };
 
-        return null;
+        return jsonMap;
     }
 
     public static String getLineDifferences(String key, String map1Value, String map2Value) {
@@ -80,10 +139,9 @@ public class Differ {
     }
 
     public static String generate(String filePath1, String filePath2) throws IOException, ParseException {
-        readMapFromFile(filePath1);
-        //Map<String, String> jsonMap1 = readMapFromJsonFile(filePath1);
-        //Map<String, String> jsonMap2 = readMapFromJsonFile(filePath2);
-        //return getMapsDifferences(jsonMap1, jsonMap2);
-        return null;
+        //readMapFromFile(filePath1);
+        Map<String, String> jsonMap1 = readMapFromFile(filePath1);
+        Map<String, String> jsonMap2 = readMapFromFile(filePath2);
+        return getMapsDifferences(jsonMap1, jsonMap2);
     }
 }
