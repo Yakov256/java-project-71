ece16d46fdc7bac381ec083dd8e61c769260ef33
yakov256 <yakov256@yandex.ru>
2023-10-17 11:49:54 +0300

    code review changes: remove the unnecessary test (getLineDifferencesStateTest)

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -1,30 +1,11 @@
 import hexlet.code.Differ;
-import hexlet.code.DiffersStates;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import static hexlet.code.Differ.getLineDifferencesState;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public final class DifferTest {
-
-    @Test
-    void getLineDifferencesStateTest() {
-        final int[] numbers1234 = {1, 2, 3, 4};
-        final int[] numbers2345 = {2, 3, 4, 5};
-        final int int123 = 123;
-        final int int234 = 234;
-
-        assertEquals(DiffersStates.notChanged, getLineDifferencesState(null, null));
-        assertEquals(DiffersStates.notChanged, getLineDifferencesState(numbers1234, numbers1234));
-        assertEquals(DiffersStates.added, getLineDifferencesState(null, "string"));
-        assertEquals(DiffersStates.added, getLineDifferencesState(null, int123));
-        assertEquals(DiffersStates.removed, getLineDifferencesState("string", null));
-        assertEquals(DiffersStates.updated, getLineDifferencesState("string1", "string2"));
-        assertEquals(DiffersStates.updated, getLineDifferencesState(int123, int234));
-        assertEquals(DiffersStates.updated, getLineDifferencesState(numbers1234, numbers2345));
-    }
 
     String getTestStr(String filename1, String filename2, String format) {
         String filepath1 = System.getProperty("user.dir") + "/src/test/resources/" + filename1;
