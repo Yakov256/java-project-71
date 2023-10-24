c9b8885307197b75733698abf7d87fff200f2489
yakov256 <yakov256@yandex.ru>
2023-10-24 16:40:07 +0300

    code review changes: refactor method of obtaining the differences of two collections

--- app/src/main/java/hexlet/code/Diff.java
+++ app/src/main/java/hexlet/code/Diff.java
@@ -11,26 +11,20 @@ public class Diff {
 
     public static List<Map<String, Object>> getTreeMapsDifferencesList(Map<String, Object> treeMap1,
                                                                        Map<String, Object> treeMap2) {
-        Object map1Value;
-        Object map2Value;
-
         Set<String> keySet = new TreeSet<>(treeMap1.keySet());
         keySet.addAll(treeMap2.keySet());
 
         List<Map<String, Object>> diffList = new ArrayList<>();
         for (String entry : keySet) {
-            map1Value = treeMap1.get(entry);
-            map2Value = treeMap2.get(entry);
-
             DiffersStates differencesState = DiffersStates.notChanged;
             if (treeMap1.containsKey(entry) & !treeMap2.containsKey(entry)) {
                 differencesState = DiffersStates.removed;
             } else if (treeMap2.containsKey(entry) & !treeMap1.containsKey(entry)) {
                 differencesState = DiffersStates.added;
-            } else if (map1Value != map2Value) {
+            } else if (treeMap1.get(entry) != treeMap2.get(entry)) {
                 differencesState = DiffersStates.updated;
-                if (map1Value != null & map2Value != null) {
-                    if (map1Value.equals(map2Value)) {
+                if (treeMap1.get(entry) != null & treeMap2.get(entry) != null) {
+                    if (treeMap1.get(entry).equals(treeMap2.get(entry))) {
                         differencesState = DiffersStates.notChanged;
                     }
                 }
@@ -39,8 +33,8 @@ public class Diff {
             Map<String, Object> diffmap = new LinkedHashMap<>();
             diffmap.put("key", entry);
             diffmap.put("Difference", differencesState);
-            diffmap.put("file1Value", map1Value);
-            diffmap.put("file2Value", map2Value);
+            diffmap.put("file1Value", treeMap1.get(entry));
+            diffmap.put("file2Value", treeMap2.get(entry));
             diffList.add(diffmap);
         }
 
